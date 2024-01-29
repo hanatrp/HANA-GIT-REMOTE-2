3334d4bb7f7541e2512d4ddd656dc53929b16f02
Hana Nur Aisyah <hananurtryn@gmail.com>
2024-01-29 12:09:29 +0700

    menambahkan list pada file hana.php

--- hana.php
+++ hana.php
@@ -8,5 +8,9 @@
 <body>
     <h1>HELOO HANA</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius debitis, animi perspiciatis quis id perferendis quasi quidem laboriosam sint molestias. Reiciendis dolorum quis ipsum unde labore illo eos quam perspiciatis.</p>
+    <ul>
+        <li>dona</li>
+        <li>dmo</li>
+    </ul>
 </body>
 </html>
